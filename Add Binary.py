a576452e5421d26105eb7370f315929a61eddfdc
Shubham <72201264+damonOP@users.noreply.github.com>
2020-10-01 16:46:46 +0530

    Update Add Binary.py

--- Add Binary.py
+++ Add Binary.py
@@ -2,3 +2,8 @@ class Solution:
     def addBinary(self, a: str, b: str) -> str:
         sum1=bin(int(a,2)+int(b,2))                     #converting the number first to ineteger and then adding both and again converting it to the binary
         return(sum1[2:])                                #returning the value after two words bcoz its already had some garbage
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        sum1=bin(int(a,2)+int(b,2))                    
+        return(sum1[2:])                               
