b909c8f292c94672f00426d09898ab2f5dc8b04e
Sachin <66303835+Sachindebug@users.noreply.github.com>
2021-10-01 13:57:38 +0530

    Added "is Subsequence"(LeetCode 392)

---
+++ Is Subsequence.cpp
@@ -0,0 +1,18 @@
+bool isSubsequence(string s, string t) {
+        int i=0;
+        int j=0;
+        while(i<s.length() && j<t.length())
+        {
+            if(s[i]==t[j])
+            {
+                j++;
+                i++;
+            }
+            else
+            {
+                j++;
+            }
+        }
+        return i==s.length();
+        
+    }
