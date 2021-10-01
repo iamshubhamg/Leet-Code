13d11aa5557d962885be0d7ff7eb6f3baf2be854
kchaithanya125 <kchaithanya125@gmail.com>
2021-10-01 16:36:53 +0530

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,6 @@
+Days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}    
+print(Days)    
+print(type(Days))    
+print("looping through the set elements ... ")    
+for i in Days:    
+    print(i)    
