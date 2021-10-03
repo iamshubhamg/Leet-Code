833826f8d8b5087813e57a27c1eaf7c05fe7eb83
Sreeja <sreeja.bhose@gmail.com>
2021-10-03 13:19:36 +0530

    Added problem 1252

--- dict.py
+++
@@ -1,6 +0,0 @@
-Days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}    
-print(Days)    
-print(type(Days))    
-print("looping through the set elements ... ")    
-for i in Days:    
-    print(i)    
--- plus.cpp
+++
@@ -1,1 +0,0 @@
-f sfs fsm fsfs
