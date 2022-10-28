9bac2b5796986e50f3788d7529453897e7964aec
kar98saxx <116908214+kar98saxx@users.noreply.github.com>
2022-10-28 22:33:57 +0530

    Create readline.cs

---
+++ readline.cs
@@ -0,0 +1,11 @@
+using System;
+public class Exercise11
+{
+  public static void Main()
+  {
+    int age;
+    Console.Write("Enter your age ");
+    age = Convert.ToInt32(Console.ReadLine());
+    Console.Write("You look younger than {0} ",age);
+  }
+}
